6ffc546a5cf34efb795dd40c2e72ea86fb146ad1
Alessandro Bonomi <alessandrobonomi14@gmail.com>
2024-01-23 15:48:12 +0100

    Parte 1: food & workout tips
    Ora inserisco il materiale nella view

--- app/src/main/java/fitness/com/MainActivity.java
+++ app/src/main/java/fitness/com/MainActivity.java
@@ -8,12 +8,19 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button8; //Target button
     private Button button9; //Target button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button8 = (Button) findViewById(R.id.button8); //Food & Workout Tips Button
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { openFoodWorkoutTips(); }
+        });
 
         button9 = (Button) findViewById(R.id.button9); //Target button
         button9.setOnClickListener(new View.OnClickListener()
@@ -23,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
                 openSetTarget();
             }
         });
+    }
+
+    public void openFoodWorkoutTips() {
+        Intent intent = new Intent(this, Food_Workout_Tips.class);
+        startActivity(intent);
     }
 
     public void openSetTarget() { //Target button
