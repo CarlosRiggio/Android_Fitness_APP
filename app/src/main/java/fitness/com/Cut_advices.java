77a62c5e48b7150f5dc9f2e22c98937dc212464b
Alessandro Bonomi <alessandrobonomi14@gmail.com>
2024-01-24 11:05:52 +0100

    Funziona il codice, ora mettiamo a posto il design

--- app/src/main/java/fitness/com/Cut_advices.java
+++ app/src/main/java/fitness/com/Cut_advices.java
@@ -3,6 +3,9 @@ package fitness.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class Cut_advices extends AppCompatActivity {
 
@@ -10,5 +13,34 @@ public class Cut_advices extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cut_advices);
+
+        String[] strings = {
+                "Incorporate high-intensity cardiovascular exercises, such as interval training, to burn more calories",
+                "Include resistance training to preserve muscle mass during weight loss",
+                "Consume smaller portions and plan frequent meals to keep metabolism active",
+                "Limit intake of added sugars and processed foods to reduce empty calories",
+                "Increase fiber intake to promote satiety and improve digestion",
+                "Stay consistently hydrated; thirst is often mistaken for hunger",
+                "Avoid skipping meals; maintain a regular eating schedule",
+                "Opt for lean protein-rich foods to support the maintenance of muscle mass",
+                "Keep a record of your meals and workouts to track progress and stay motivated",
+                "Limit consumption of calorie-laden beverages, favoring water and unsweetened tea",
+                "Experiment with thermogenic foods like chili and ginger to boost metabolism",
+                "Prepare healthy meals in advance to avoid impulsive food choices",
+                "Consult with a nutritionist for a personalized weight loss meal plan",
+                "Incorporate interval training exercises to maximize post-workout calorie burn",
+                "Focus on quality sleep; lack of sleep can impact weight loss",
+                "Limit alcohol intake, as it can interfere with fat metabolism",
+                "Eat mindfully; slow down and savor each bite to prevent overeating",
+                "Avoid eating too close to bedtime to facilitate digestion",
+                "Include omega-3-rich foods like salmon to promote fat burning",
+                "Seek the support of a personal trainer or group to stay motivated during the weight loss journey"
+        };
+
+        // Genera un numero casuale tra 0 e 19
+        int randomNumber = new Random().nextInt(20);
+
+        // Imposta il testo della TextView con la stringa casuale
+        ((TextView) findViewById(R.id.textView)).setText(strings[randomNumber]);
     }
 }
