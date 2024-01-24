4712b02433d41a96d37390359c0cd6dc8bda9a47
Alessandro Bonomi <alessandrobonomi14@gmail.com>
2024-01-24 10:54:06 +0100

    La roba random funziona,ora la mettiamo a posto io e Riggio

--- app/src/main/java/fitness/com/Bulk_advices.java
+++ app/src/main/java/fitness/com/Bulk_advices.java
@@ -3,6 +3,9 @@ package fitness.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class Bulk_advices extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class Bulk_advices extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bulk_advices);
+
+        // Crea un array delle stringhe
+        String[] strings = {"AA AA AA AA", "BB BB BB BB", "CC CC CC CC", "DD DD DD DD"};
+
+        // Genera un numero casuale tra 0 e 3
+        int randomNumber = new Random().nextInt(4);
+
+        // Imposta il testo della TextView con la stringa casuale
+        ((TextView) findViewById(R.id.textView)).setText(strings[randomNumber]);
+
+
     }
 }
